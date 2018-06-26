ec15dc1fae2ba392c01e45168e9adf9c514a25d5
Beibei <beibei_jiang@psych.mpg.de>
2018-06-26 16:53:15 +0200

    make much more comments on kNN

--- chapter2/kNN.py
+++ chapter2/kNN.py
@@ -9,15 +9,21 @@ def createDataSet():
 
 # inX is input vector; dataSet is training set; k is the number of selected nearest neighbors
 def classify0(inX, dataSet, labels, k):
-    dataSetSize = dataSet.shape[0]
-    diffMat = tile(inX, (dataSetSize, 1)) - dataSet
-    sqDiffMat = diffMat**2
-    sqDistances = sqDiffMat.sum(axis=1)
-    distances = sqDistances**0.5
-    sortedDistIndicies = distances.argsort()
+    dataSetSize = dataSet.shape[0]     # .shape return a vector and get the first one. it is the number of row
+
+	# get the distance (each row of dataSet is a class)
+    diffMat = tile(inX, (dataSetSize, 1)) - dataSet  # tile([0,0], (5,1)) means repeat [0,0] 5 times in row and 1 time in column
+
+    sqDiffMat = diffMat**2 # **2 square of each element
+    sqDistances = sqDiffMat.sum(axis=1)    # axis＝0 works on each column (summarize all row elements)，axis＝1 works on each row (summarize all column elements)
+    distances = sqDistances**0.5  # **0.5 square root of each element
+    sortedDistIndicies = distances.argsort()   # argsort returns the indices that would sort an array
     classCount = {}
     for i in range(k):
         voteIlabel = labels[sortedDistIndicies[i]]
         classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
     sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]
+
+
+
